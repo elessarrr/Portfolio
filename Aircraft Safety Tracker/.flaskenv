ed57be1fa7a623a65e2c38115c48c51644ecfb14
Bhavesh R <bapuis@gmail.com>
2026-05-09 22:16:52 +0800

    Untrack Aircraft Safety Tracker directory

--- Aircraft Safety Tracker/.flaskenv
+++
@@ -1,3 +0,0 @@
-FLASK_RUN_PORT=5001
-FLASK_APP=app
-FLASK_ENV=development
--- Aircraft Safety Tracker/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email, Optional
-
-class RequestDataForm(FlaskForm):
-    aircraft_model = StringField('Aircraft Model', validators=[DataRequired()])
-    email = StringField('Your Email (Optional)', validators=[Optional(), Email()])
-    submit = SubmitField('Request Data')
